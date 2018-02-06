e59ca79fcf15fda4512050a28dc9de383f3e2451
tyleran <AnT3384@sduhsd.lan>
2018-02-06 10:06:24 -0800

    Added Unit 0,1

---
+++ Unit0/src/Lab0a3.java
@@ -0,0 +1,29 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Lab0a3
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Your Name \t  Date \t Period\n\n" );
+		System.out.println("What type of ANIMAL YOU WILL DRAW" );
+		System.out.println("\n\n\n\n" );
+		System.out.println("             \\_____/");
+		System.out.println("        ______\\___/______ ");
+		System.out.println("        |    |-    -|    |");
+		System.out.println("        |       /\\       |");
+		System.out.println("        |      /  \\      |" );
+		System.out.println("        |     /    \\     |" );
+		System.out.println("        |    [------]    |" );
+		System.out.println("        |________________|");
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" );
+	}
+}
---
+++ Unit0/src/Lab0c.java
@@ -0,0 +1,69 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class Lab0c
+{
+	public static void main (String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+
+
+		int intOne, intTwo;
+		double doubleOne, doubleTwo;
+		float floatOne, floatTwo;
+		short shortOne, shortTwo;
+
+
+		System.out.print("Enter an integer :: ");
+		intOne = keyboard.nextInt();
+
+
+		System.out.print("Enter an integer :: ");
+		intTwo = keyboard.nextInt();
+
+
+		//add in input for all variables
+    System.out.print("Enter a double :: ");
+    doubleOne = keyboard.nextDouble();
+    
+    System.out.print("Enter a double :: ");
+    doubleTwo = keyboard.nextDouble();
+    
+    System.out.print("Enter a float :: ");
+    floatOne = keyboard.nextFloat();
+    
+    System.out.print("Enter a float :: ");
+    floatTwo = keyboard.nextFloat();
+    
+    System.out.print("Enter a short :: ");
+    shortOne = keyboard.nextShort();
+    
+    System.out.print("Enter a short :: ");
+    shortTwo = keyboard.nextShort();
+    
+
+
+		//add in output for all variables
+
+		System.out.println();
+		System.out.println("integer one = " + intOne );
+		System.out.println("integer two = " + intTwo +" \n");
+		System.out.println("double one = " + doubleOne );
+		System.out.println("double two = "+ doubleTwo);
+		System.out.println();
+		System.out.println("float one = " +floatOne);
+		System.out.println("float two = " +floatTwo);
+		System.out.println();
+		System.out.println("short one = " +shortOne);
+		System.out.println("short two = " +shortTwo);
+
+
+
+	}
+}
